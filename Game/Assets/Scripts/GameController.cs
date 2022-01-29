c68b21ad7960f83a08a3c8c3ff320a6fde92cf9a
William Terry <Tezza48is@aol.com>
2022-01-29 21:16:58 +0000

    Player mesh generation done

--- Game/Assets/Scripts/GameController.cs
+++ Game/Assets/Scripts/GameController.cs
@@ -32,6 +32,7 @@ public class GameController : MonoBehaviour
 
     public float camXOffset = 10.0f;
     public float camYOffset = 0.0f;
+    public float camYOffsetOutside = 0.0f;
 
     Coroutine tween;
 
@@ -112,19 +113,20 @@ public class GameController : MonoBehaviour
         player.transform.rotation = Quaternion.AngleAxis(Mathf.Rad2Deg * playerX, Vector3.forward);
 
         Vector3 newPos;
-        // ! Not really too bothered about having the camera centererd when inside, this way we'll get some nice camera movement
-        //if (isInside)
-        //{
-        //    newPos = new Vector3(0.0f, 0.0f, player.transform.position.z - camOffset);
-        //} else
-        //{
-        //}
+        // !Not really too bothered about having the camera centererd when inside, this way we'll get some nice camera movement
+        if (isInside)
+        {
+            newPos = new Vector3(Mathf.Sin(playerX) * (offset + camYOffset * offsetDirection), -Mathf.Cos(playerX) * (offset + camYOffset * offsetDirection), player.transform.position.z - camXOffset);
+
+        }
+        else
+        {
+            newPos = new Vector3(Mathf.Sin(playerX) * (offset + camYOffsetOutside * offsetDirection), -Mathf.Cos(playerX) * (offset + camYOffsetOutside * offsetDirection), player.transform.position.z - camXOffset);
+        }
 
         // if moving left, offset camera left a bit more, vice versa.
 
         // TODO WT: Camera shake?
-
-        newPos = new Vector3(Mathf.Sin(playerX) * (offset + camYOffset * offsetDirection), -Mathf.Cos(playerX) * (offset + camYOffset * offsetDirection), player.transform.position.z - camXOffset);
 
         camera.transform.position = Vector3.Lerp(camera.transform.position, newPos, Time.deltaTime * 10.0f);
         camera.transform.rotation = player.transform.rotation;
